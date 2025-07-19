27930df22a34389346ed95aa804be98156d7e164
anthony.dke@gmail.com <anthonydike@nyu.edu>
2025-07-19 14:09:00 -0400

    feat: langfuse integration

--- src/env.js
+++ src/env.js
@@ -26,6 +26,9 @@ export const env = createEnv({
         ? z.string()
         : z.string().optional(),
     SERPER_API_KEY: z.string(),
+    LANGFUSE_SECRET_KEY: z.string(),
+    LANGFUSE_PUBLIC_KEY: z.string(),
+    LANGFUSE_BASEURL: z.string().url(),
   },
 
   /**
@@ -48,6 +51,9 @@ export const env = createEnv({
     AUTH_DISCORD_ID: process.env.AUTH_DISCORD_ID,
     AUTH_DISCORD_SECRET: process.env.AUTH_DISCORD_SECRET,
     SERPER_API_KEY: process.env.SERPER_API_KEY,
+    LANGFUSE_SECRET_KEY: process.env.LANGFUSE_SECRET_KEY,
+    LANGFUSE_PUBLIC_KEY: process.env.LANGFUSE_PUBLIC_KEY,
+    LANGFUSE_BASEURL: process.env.LANGFUSE_BASEURL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
